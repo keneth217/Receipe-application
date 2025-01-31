71d391a57fd537c070067a969c4786adca7fa911
keneth/217 <kipyegonkeneth03@gmail.com>
2025-01-31 14:39:04 +0300

    Add retrofit api call,design categories screen

---
+++ app/src/main/java/com/keneth/receipe/ApiService.kt
@@ -0,0 +1,17 @@
+package com.keneth.receipe
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+private val retrofit = Retrofit.Builder().baseUrl("https://www.themealdb.com/api/json/v1/1/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+val reciepService = retrofit.create(ApiService::class.java)
+
+interface ApiService {
+    @GET("categories.php")
+    suspend fun getCategories(): CategoriesResponse
+
+
+}
