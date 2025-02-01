b46a031d134b80680e8f4e07f99cec7b45969690
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-01 14:38:13 +0300

    navigation  to screens,pass data to another screen

--- app/src/main/java/com/keneth/receipe/MainActivity.kt
+++ app/src/main/java/com/keneth/receipe/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.keneth.receipe
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,16 +12,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.keneth.receipe.ui.theme.ReceipeApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             ReceipeApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CategoriesScreen(
+
+
+                    CategoryApp(
+                        navController = navController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
